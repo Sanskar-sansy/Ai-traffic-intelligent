14426653fb35e175c5408762bfa8b6f7afe6ad57
Sanskar Saxena <manusaxena365@gmail.com>
2026-04-24 11:41:34 +0530

    Update weather_service.py

--- backend/services/weather_service.py
+++ backend/services/weather_service.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "4278f03a8620080c9e297fe5b3ca84f3"
+API_KEY = ""
 
 def get_weather(city: str):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
